9d70b631b61cbd61903a7eb7e147b368ca8418d7
Mateo Graber <grabermn3@gmail.com>
2025-03-19 01:01:36 -0400

    commit on "main"

--- src/components/Rulers.js
+++ src/components/Rulers.js
@@ -9,7 +9,9 @@ function Rulers({ rulersImagesArray, moveToRulers, setMoveToRulers }) {
     const mousePosXDiff = useRef(0)
     const mousePosYDiff = useRef(0)
     const rulersRulesIconRef = useRef(null)
-    const settingsIconRef = useRef(null)
+    const rulersRulesBox = useRef(null)
+    const rulersSettingsIconRef = useRef(null)
+    const rulersSettingsBox = useRef(null)
     const rulerTitle = useRef('inchesRuler')
     const rulersTitleRef = useRef(null)
     const rulerRef = useRef(null)
@@ -48,28 +50,31 @@ function Rulers({ rulersImagesArray, moveToRulers, setMoveToRulers }) {
         }
     }
     const animateElements = () => {
-        const rulersElements = [rulersTitleRef, rulersRulesIconRef, rulerRef]
+        const rulersElements = [rulersTitleRef, rulersRulesIconRef, rulersRulesBox, rulerRef]
         for (let i = 0; i < rulersElements.length; i++) {
             readyForAnimation(rulersElements[i])
         }
         rulersTitleRef.current.style.animationName = 'appearFromRight'
-        rulerRef.current.style.animationName = 'appearFromRightRuler'
         rulersRulesIconRef.current.style.animationName = 'appearFromRightRulersRules'
-    }
-    const readyForMove = (element) => {
-        element.current.style.opacity = '0'
+        rulersRulesBox.current.style.animationName = 'appearFromRightRulersRules'
+        rulerRef.current.style.animationName = 'appearFromRightRuler'
     }
     const readyForAnimation = (element) => {
         element.current.style.animationName = 'none'
         element.current.classList.remove('readyForAnim')
         element.current.classList.add('readyForAnim')
     }
+    const readyForMove = (element) => {
+        element.current.style.opacity = '0'
+    }
     useEffect(() => {
         if (moveToRulers) {
             rulersTitleRef.current.style.display = 'block'
             rulersRulesIconRef.current.style.display = 'inline'
+            rulersRulesBox.current.style.display = 'inline'
+            rulersRulesBox.current.style.visibility = 'hidden'
             rulerRef.current.style.display = 'block'
-            const rulersElements = [rulersTitleRef, rulersRulesIconRef, rulerRef]
+            const rulersElements = [rulersTitleRef, rulersRulesIconRef, rulersRulesBox, rulerRef]
             for (let i = 0; i < rulersElements.length; i++) {
                 readyForMove(rulersElements[i])
             }
@@ -82,12 +87,20 @@ function Rulers({ rulersImagesArray, moveToRulers, setMoveToRulers }) {
     }, [moveToRulers])
     useEffect(() => {
         rulersTitleRef.current.style.display = 'none'
-        rulerRef.current.style.display = 'none'
         rulersRulesIconRef.current.style.display = 'none'
+        rulersRulesBox.current.style.display = 'none'
+        rulerRef.current.style.display = 'none'
     }, [])
     return (
         <div>
-            <img onClick='{() => openOrClose(rulesBox)}' id='rulersRulesIcon' src={rulesIcon} ref={rulersRulesIconRef} alt='rulersRulesIcon' title="rulersRules" />
+            <img onClick={() => openOrClose(rulersRulesBox)} id='rulersRulesIcon' src={rulesIcon} ref={rulersRulesIconRef} alt='rulersRulesIcon' title="rulersRules" />
+            <span id='rulersRulesBox' ref={rulersRulesBox}>
+                <div id='rulesTitle'>Rules</div>
+                <ul>
+                    <li></li>
+                    <li id='lvl2li'></li>
+                </ul>
+            </span>
             <div id='rulersTitle' ref={rulersTitleRef}>Rulers</div>
             <img id='ruler' src={inchesRuler} ref={rulerRef} alt={rulerTitle.current} title={rulerTitle.current}/>
         </div>
