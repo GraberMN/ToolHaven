e1ac2f40de2bafb02acc80705430d91aa8d8b742
Mateo Graber <grabermn3@gmail.com>
2025-06-15 22:21:47 -0400

    Added Paintbrush RulesIcon & RulesBox

--- src/components/Paintbrush.js
+++ src/components/Paintbrush.js
@@ -4,9 +4,104 @@ import './paintbrush.css';
 
 function Paintbrush({ paintbrushImagesArray, moveToPaintbrush, setMoveToPaintbrush }) {
     const [settingsIcon, rulesIcon, homeButton] = paintbrushImagesArray;
+    const paintbrushRulesIconRef = useRef(null);
+    const paintbrushRulesBox = useRef(null);
+    const openOrClose = (element) => {
+        if (element.current.style.visibility === "hidden") {
+            element.current.style.visibility = "visible";
+        } else {
+            element.current.style.visibility = "hidden";
+        }
+    }
+    const onAdjustWindowWidthPaintbrush = () => {
+        if (window.innerWidth <= 740) {
+            paintbrushRulesIconRef.current.style.transform = 'translateX(-260px)';
+            paintbrushRulesBox.current.style.transform = 'translateX(-260px)';
+        } else {
+            paintbrushRulesIconRef.current.style.transform = 'translateX(-367px)';
+            paintbrushRulesBox.current.style.transform = 'translateX(-367px)';
+        }
+    }
+    const animateElements = () => {
+        const paintbrushElements = [paintbrushRulesIconRef, paintbrushRulesBox];
+        for (let i = 0; i < paintbrushElements.length; i++) {
+            readyForAnimation(paintbrushElements[i]);
+        }
+
+        if (window.innerWidth > 740) {
+            paintbrushRulesIconRef.current.style.animationName = 'appearFromRightPaintbrushRules';
+            paintbrushRulesBox.current.style.animationName = 'appearFromRightPaintbrushRules';
+        } else {
+            paintbrushRulesIconRef.current.style.animationName = 'appearFromRightPaintbrushRulesSmall';
+            paintbrushRulesBox.current.style.animationName = 'appearFromRightPaintbrushRulesSmall';
+        }
+
+    }
+    const readyForAnimation = (element) => {
+        element.current.style.animationName = 'none';
+        element.current.classList.remove('readyForAnim');
+        element.current.classList.add('readyForAnim');
+    }
+    const readyForMove = (element) => {
+        element.current.style.opacity = '0';
+    }
+    useEffect(() => {
+        if (moveToPaintbrush) {
+            document.body.style.pointerEvents = 'none';
+            paintbrushRulesIconRef.current.style.display = 'inline';
+            paintbrushRulesBox.current.style.display = 'inline';
+            paintbrushRulesBox.current.style.visibility = 'hidden';
+            const paintbrushElements = [paintbrushRulesIconRef, paintbrushRulesBox];
+            for (let i = 0; i < paintbrushElements.length; i++) {
+                readyForMove(paintbrushElements[i]);
+            }
+            document.body.style.backgroundColor = 'hsl(272, 20.50%, 85.70%)';
+            setTimeout(() => {
+                animateElements();
+            }, 2000);
+            setTimeout(() => {
+                const paintbrushNonPaintbrushElements = [paintbrushRulesIconRef, paintbrushRulesBox];
+                for (let i = 0; i < paintbrushNonPaintbrushElements.length; i++) {
+                    paintbrushNonPaintbrushElements[i].current.style.animationName = 'none';
+                    paintbrushNonPaintbrushElements[i].current.style.opacity = '100';
+                }
+                window.addEventListener("resize", onAdjustWindowWidthPaintbrush);
+                document.body.style.pointerEvents = 'auto';
+                if (window.innerWidth <= 740) {
+                    paintbrushRulesIconRef.current.style.transform = 'translateX(-260px)';
+                    paintbrushRulesBox.current.style.transform = 'translateX(-260px)';
+                }
+            }, 4000);
+        }
+    }, [moveToPaintbrush]);
+    useEffect(() => {
+            const goneElements = [paintbrushRulesIconRef, paintbrushRulesBox];
+            for (let i = 0; i < goneElements.length; i++) {
+                goneElements[i].current.style.display = 'none';
+            }
+            return () => {
+                window.removeEventListener("resize", onAdjustWindowWidthPaintbrush);
+            }
+        }, []);
     return (
         <div>
-            
+            <img onClick={() => openOrClose(paintbrushRulesBox)} id='paintbrushRulesIcon' draggable={false} src={rulesIcon} ref={paintbrushRulesIconRef} alt='paintbrushRulesIcon' title="paintbrushRules" />
+            <span id='paintbrushRulesBox' draggable={false} ref={paintbrushRulesBox}>
+                <div id='paintbrushRulesTitle' draggable={false}>Rules</div>
+                <ul>
+                    <li>Hover over the Paintbrush canvas or any button to find out what it is/represents.</li>
+                    <li>The bottom left Home button takes you back to the Home page.</li>
+                    <li>The Settings tab lets you .</li>
+                    <li>The supported file extensions for download are .</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li>When changing color in Settings, drag the pointer around for it to work seamlessly.</li>
+                    <li>Paint Tool-Specific Rules:</li>
+                    <li id='lvl2li'></li>
+                    <li id='lvl2li'></li>
+                </ul>
+            </span>
         </div>
     )
 }
